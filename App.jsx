b4286016360b318c2d7d1fd332286327565cd175
coralPK <127463647+coralpk@users.noreply.github.com>
2023-07-27 14:38:06 -0400

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Authenitcate from './components/Authenitcate'
+import SignUpForm from './components/SignUpForm'
+
+function App() {
+  return (
+    <>
+      <SignUpForm />
+      <Authenitcate />
+    </>
+  )
+}
+
+export default App
